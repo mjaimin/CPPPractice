92550f2df2e8761047540e85a604f106e261e09c
m.jaimin@gmail.com <m.jaimin@gmail.com>
2013-06-14 23:00:38 +0900

    dont know what changed but same repo was accessed from Linux Box

---
+++ WorkOnThis/Hypotheticle/callingPureVirtualAtRunTime.cpp
@@ -0,0 +1,27 @@
+//Calls Pure Virtual function at runTime
+#include <pthread.h>
+#include <unistd.h>
+struct base
+{
+    virtual ~base() { sleep(1); }
+    virtual void func() = 0;
+};
+struct derived : public base
+{
+    virtual ~derived() { }
+    virtual void func() { return; }
+};
+static void *thread_func(void* v)
+{
+    base *b = reinterpret_cast<base*>(v);
+    while (true) b->func();
+    return 0;
+}
+int main()
+{
+    pthread_t t;
+    base *b = new derived();
+    pthread_create(&t, 0, thread_func, b);
+    delete b;
+    return 0;
+}
