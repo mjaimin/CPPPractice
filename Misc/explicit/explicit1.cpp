0ef214ecd54e2a91d8fb48628efd8fd61187429c
Jaimin <m.jaimin@gmail.com>
2013-12-30 16:32:39 +0530

    Explanation put in comments

--- Misc/explicit/explicit1.cpp
+++ Misc/explicit/explicit1.cpp
@@ -7,13 +7,13 @@ void f(A) {}
 
 int main()
 {
-	A a1 = 37;      // illegal
+	A a1 = 37;      // illegal - if not explicit, it would call only A(int) 
 
 	A a2 = A(47);   // OK
 
 	A a3(57);       // OK
 
-	a1 = 67;        // illegal
+	a1 = 67;        // illegal - if not explicit it would call A(int) and =assignment operator
 
-	f(77);          // illegal
+	f(77);          // illegal - if not explicit, it would call A(int) and then copy constructor should get called
 }
