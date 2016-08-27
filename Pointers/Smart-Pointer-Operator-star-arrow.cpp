6e5094888ebb995011087d9fc46593d19a5226cf
mjaimin <m.jaimin@gmail.com>
2016-08-27 13:39:42 +0530

    Update Smart-Pointer-Operator-star-arrow.cpp

--- Pointers/Smart-Pointer-Operator-star-arrow.cpp
+++ Pointers/Smart-Pointer-Operator-star-arrow.cpp
@@ -1,48 +1,32 @@
 #include <iostream>
 #include <string>
+
 template <class T>
-class SM
-{
-        T *nativeObj;
-        public:
-        SM( T* obj )
-        {
-                nativeObj = obj;
-        }
-        ~SM()
-        {
-                delete nativeObj;
-        }
-        T& operator*()
-        {
-                return (*nativeObj);
-        }
-        T* operator->()
-        {
-                return nativeObj;
-        }
+class SM {
+    T *nativeObj;
+    public:
+    SM( T* obj ) { nativeObj = obj; }
+    ~SM() { delete nativeObj; }
+    T& operator*() { return (*nativeObj); }
+    T* operator->() { return nativeObj; }
 };
-class Student
-{
-        std::string Name;
-        public:
-        Student( std::string name ): Name(name)
-        {
-                std::cout << Name << " Object Created" << std::endl;
-        }
-        ~Student()
-        {
-                std::cout << Name << " Object Destructed" << std::endl;
-        }
-        std::string getName()
-        {
-                return Name;
-        }
+
+class Student {
+    std::string Name;
+    public:
+    Student( std::string name ): Name(name)
+    { std::cout << Name << " Object Created" << std::endl; }
+
+    ~Student() 
+    { std::cout << Name << " Object Destructed" << std::endl; }
+
+    std::string getName() { return Name; }
 };
+
 int main()
 {
-        Student* s = new Student("Sooraj");
-        SM<Student> sm(s);
-        std::cout << sm->getName() << std::endl;
-        std::cout << (*sm).getName() << std::endl;
+    Student* s = new Student("Sooraj");
+    SM<Student> sm(s);
+    std::cout << sm->getName() << std::endl;
+    std::cout << (*sm).getName() << std::endl;
 }
