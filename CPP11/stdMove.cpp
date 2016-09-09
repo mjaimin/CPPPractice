9feb28bf94f80309deefb5d371622fc34173265a
mjaimin <m.jaimin@gmail.com>
2016-09-09 15:02:28 +0530

    Create stdMove.cpp

---
+++ CPP11/stdMove.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+ 
+int main()
+{
+    std::string str1 = "Hello";
+    std::string str2 = "";
+     
+    std::cout << "str1 before copy:" << str1 << std::endl;
+	std::cout << "str2 before copy:" << str2 << std::endl;
+	str2=std::move(str1);
+    std::cout << "str1 after copy:" << str1 << std::endl;
+	std::cout << "str2 after copy:" << str2 << std::endl;
+}
